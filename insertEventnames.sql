723bbb859d3dbcf85414b8c4c3d25d3bc3a76b3e
chocopndn <chocopndn@gmail.com>
2025-01-28 00:26:38 +0800

    Added event names

--- insertEventnames.sql
+++ insertEventnames.sql
@@ -1,4 +1,12 @@
 INSERT INTO event_names (event_name)
 VALUES 
     ('UCV@76th Foundation Week'),
-    ('IT Day')
+    ('IT Day'),
+    ('Programming Bootcamp'),
+    ('Database Management Workshop'),
+    ('AI and Machine Learning Seminar'),
+    ('Web Development Week'),
+    ('Cybersecurity Awareness Campaign'),
+    ('Coding Hackathon'),
+    ('Tech Talk: Future of IT'),
+    ('Game Development Jam');
